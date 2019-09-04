f96015c14ddf051413b8ba139f884434ce68d9d7
Daniel-Stafford <stafford.daniel@gmail.com>
2019-09-04 15:52:45 +0300

    6.10 Better anecdotes, step8

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { addVote } from '../reducers/anecdoteReducer'
+import {
+  setNotification,
+  removeNotification
+} from '../reducers/notificationReducer'
 
 const AnecdoteList = props => {
   const anecdotes = props.store.getState().anecdotes
 
-  const handleVote = id => {
-    console.log('vote', id)
-    props.store.dispatch(addVote(id))
+  const handleVote = anecdote => {
+    console.log('vote', anecdote)
+    props.store.dispatch(addVote(anecdote.id))
+    props.store.dispatch(setNotification(`You voted for: ${anecdote.content}`))
+    console.log('first dispatch')
+    setTimeout(() => props.store.dispatch(removeNotification()), 5000)
   }
 
   return (
@@ -16,7 +23,7 @@ const AnecdoteList = props => {
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote.id)}>vote</button>
+            <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
       ))}
--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,18 +1,24 @@
-const initialNotification = 'This is the default message'
-
-const notificationReducer = (state = initialNotification, action) => {
+const notificationReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_NOTIFICATION':
       return action.content
+    case 'REMOVE_NOTIFICATION':
+      return ''
     default:
       return state
   }
 }
 
-export const notificationChange = content => {
+export const setNotification = content => {
   return {
     type: 'SET_NOTIFICATION',
     content
+  }
+}
+
+export const removeNotification = () => {
+  return {
+    type: 'REMOVE_NOTIFICATION'
   }
 }
 
